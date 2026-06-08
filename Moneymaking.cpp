65db9d495caae70b997d678ebb91dabf30818b08
Md: Sabbir Hossain Sourav <mdsabbirhossainsourav15@gmail.com>
2026-06-08 19:08:02 +0600

    Add files via upload

---
+++ Moneymaking.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    long long x,y;
+    cin >> x >> y;
+    long long nugts = x*5000;
+    long long srs = y*9800;
+    long long tl = nugts + srs;
+    cout << tl << endl;
+    return 0;
+}
